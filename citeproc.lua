bcf8c66b196932b886e5cab6bdfdb63a8f7ae282
Zeping Lee <zepinglee@gmail.com>
2021-09-13 14:47:43 +0800

    Rename files according to CTAN policies

--- citeproc.lua
+++
@@ -1,9 +0,0 @@
---[[
-  Copyright (C) 2021 Zeping Lee
---]]
-
--- The kpse library replaces package.searchers[2] with its own loader function
--- which cannot find `./?/init.lua`.
--- Thus this file is required to load `./citeproc/init.lua`.
-
-return require("citeproc.init")
