8e194acbc2c3b844998bbdd8d5d6aafe3ff605b2
Zeping Lee <zepinglee@gmail.com>
2021-12-15 18:32:27 +0800

    Remove bib-data option

--- build.lua
+++ build.lua
@@ -10,7 +10,7 @@ testfiledir = "./test/latex"
 testsuppdir = testfiledir .. "/support"
 
 exefiles = {"**/citeproc"}
-installfiles = {"**/*.sty", "**/*.lua", "**/*.json", "**/csl-locales-*.xml", "**/*.csl"}
+installfiles = {"**/*.sty", "**/*.lua", "**/citeproc", "**/*.json", "**/csl-locales-*.xml", "**/*.csl"}
 scriptfiles = {"**/*.lua", "**/citeproc"}
 -- scriptmanfiles = {"citeproc.1"}
 sourcefiles = {"citeproc/*", "latex/*", "locales/csl-locales-*.xml", "styles/*.csl"}
