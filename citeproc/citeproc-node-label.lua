a4b461afc5ccddb8386dcef296d75881f2805123
Zeping Lee <zepinglee@gmail.com>
2022-06-24 18:49:05 +0800

    Fix label_EmptyLabelVanish.txt

--- citeproc/citeproc-node-label.lua
+++ citeproc/citeproc-node-label.lua
@@ -48,7 +48,7 @@ function Label:build_ir(engine, state, context)
     variable = context:get_variable("label")
   end
   local text = context:get_simple_term(variable, self.form, is_plural)
-  if not text then
+  if not text or text == "" then
     return nil
   end
 
@@ -84,100 +84,6 @@ function Label:_is_variable_plural(variable, context)
     end
   end
   return false
-end
-
-function Label:render (item, context)
-  self:debug_info(context)
-  context = self:process_context(context)
-
-  local variable_name
-  if context.names_element then
-    -- The `variable` attribute of names may hold multiple roles.
-    -- Each of them may call `Label:render()` to render the term.
-    -- When used in `names` element, the role name is the first argument
-    -- and the item is accessed via `context.item`.
-    -- Bad design
-    -- TODO: Redesign the arguments of render()
-    variable_name = item
-  else
-    variable_name = context.options["variable"]
-  end
-
-  local form = context.options["form"]
-  local plural = context.options["plural"] or "contextual"
-
-  if not context.names_element then
-    local variable_type = util.variable_types[variable_name]
-    -- variable must be or one of the number variables.
-    if variable_type ~= "number" then
-      return nil
-    end
-    -- The term is only rendered if the selected variable is non-empty
-    local variable = item[variable_name]
-    if not variable then
-      return nil
-    end
-    if type(variable) == "string" then
-      if not (string.match(variable, "^%d") or util.is_numeric(variable)) then
-        return nil
-      end
-    end
-  end
-
-  local term
-  if variable_name == "locator" then
-    local locator_type = item.label
-    term = self:get_term(locator_type, form)
-  else
-    term = self:get_term(variable_name, form)
-  end
-
-  local res = nil
-  if term then
-    if plural == "contextual" and self:_is_plural(variable_name, context) or plural == "always" then
-      res = term:render(context, true)
-    else
-      res = term:render(context, false)
-    end
-
-    res = self:_apply_strip_periods(res, context)
-    res = self:_apply_case(res, context)
-    res = self:_apply_format(res, context)
-    res = self:_apply_affixes(res, context)
-  end
-  return res
-end
-
-function Label:_is_plural (variable_name, context)
-  local variable_type = util.variable_types[variable_name]
-  -- Don't use self:get_variable here
-  local variable = context.item[variable_name]
-  local res = false
-  if variable_type == "name" then
-    -- Label inside `names`
-    res = #variable > 1
-
-  elseif variable_type == "number" then
-    if util.startswith(variable_name, "number-of-") then
-      res = tonumber(variable) > 1
-    else
-      variable = tostring(variable)
-      variable = string.gsub(variable, "\\%-", "")
-      if #util.split(variable, "%s*[,&-]%s*") > 1 then
-        -- check if contains multiple numbers
-        -- "i–ix": true
-        -- res = string.match(tostring(variable), "%d+%D+%d+") ~= nil
-        res = true
-      elseif string.match(variable, "%Aand%A") or string.match(variable, "%Aet%A") then
-        res = true
-      else
-        res = false
-      end
-    end
-  else
-    util.warning("Invalid attribute \"variable\".")
-  end
-  return res
 end
 
 
