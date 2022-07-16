889ff5256a93db0dd4f6671b7466339e2a006682
Zeping Lee <zepinglee@gmail.com>
2022-07-16 11:00:33 +0800

    Fix bugreports_MovePunctuationInsideQuotesForLocator.txt

--- citeproc/citeproc-node-label.lua
+++ citeproc/citeproc-node-label.lua
@@ -46,6 +46,10 @@ function Label:build_ir(engine, state, context)
   local variable = self.variable
   if variable == "locator" then
     variable = context:get_variable("label") or "page"
+    if variable == "sub verbo" then
+      -- bugreports_MovePunctuationInsideQuotesForLocator.txt
+      variable = "sub-verbo"
+    end
   end
   local text = context:get_simple_term(variable, self.form, is_plural)
   if not text or text == "" then
